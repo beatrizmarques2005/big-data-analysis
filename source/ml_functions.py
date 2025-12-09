26cf61ab5d6b33b06ec9fc931d07f36e7d09b2dc
David Carrilho <20231693@novaims.unl.pt>
2025-12-09 13:54:34 +0000

    modelling start and ml functions

--- source/ml_functions.py
+++ source/ml_functions.py
@@ -1,1 +1,37 @@
-# CODE
+from pyspark.ml.evaluation import (
+    BinaryClassificationEvaluator,
+    MulticlassClassificationEvaluator
+)
+import pyspark.sql.functions as F
+
+
+# ------------------------------
+# 1. ROC-AUC
+# ------------------------------
+def get_auc(preds, label_col="label", raw_pred_col="rawPrediction"):
+    evaluator = BinaryClassificationEvaluator(
+        labelCol=label_col,
+        rawPredictionCol=raw_pred_col,
+        metricName="areaUnderROC"
+    )
+    return evaluator.evaluate(preds)
+
+
+# ------------------------------
+# 2. F1 Score
+# ------------------------------
+def get_f1(preds, label_col="label", pred_col="prediction"):
+    evaluator = MulticlassClassificationEvaluator(
+        labelCol=label_col,
+        predictionCol=pred_col,
+        metricName="f1"
+    )
+    return evaluator.evaluate(preds)
+
+
+# ------------------------------
+# 3. Confusion Matrix
+# ------------------------------
+def get_confusion_matrix(preds, label_col="label", pred_col="prediction"):
+    cm = preds.groupBy(label_col, pred_col).count().orderBy(label_col, pred_col)
+    return cm
